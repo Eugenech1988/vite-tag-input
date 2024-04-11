612d77422ef706f4a83e28103c2dee5b2b1fac1b
Yevhenii Chunachenko <yevhenii.chumachenko@norse.digital>
2024-04-11 20:14:09 +0300

    special character fix

--- src/components/TagsInput/index.tsx
+++ src/components/TagsInput/index.tsx
@@ -10,11 +10,14 @@ type TTagsInputProps = {
 const TagsInput: FC<TTagsInputProps> = ({data}) => {
   const {tagsList, addTag, setStringValue, searchString, suggestions, setSuggestions, deleteTag} = useTagsStore();
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const specialChar = /[@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    const specialIndex = e.target.value.search(specialChar);
+    const nextSubstring = e.target.value.substring(specialIndex + 1);
     setStringValue(e.target.value);
     if (e.target.value) {
       setSuggestions(
         data
-          .filter((item: any) => (item.name.toLowerCase().includes(e.target.value)))
+          .filter((item: any) => (item.name.toLowerCase().includes((specialChar && specialIndex === 0 && nextSubstring !== '') ? nextSubstring : e.target.value)))
           .filter((item: any) => !tagsList.includes(item.name))
       );
     } else if (!e.target.value) {
